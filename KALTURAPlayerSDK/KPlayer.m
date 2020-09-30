cc71b169b0d0fbaabb592226b86ce5b65a2e0144
Sam Easterby-Smith <me@samscam.co.uk>
2020-09-30 10:02:17 +0100

    Playback buffer keypaths were throwing warnings

--- KALTURAPlayerSDK/KPlayer.m
+++ KALTURAPlayerSDK/KPlayer.m
@@ -782,9 +782,9 @@ NSString * const StatusKeyPath = @"status";
                                                       [weakSelf playerHanging];
                                                   }];
     
-    playbackBufferEmptyKeyPath = NSStringFromSelector(@selector(playbackBufferEmpty));
-    playbackLikelyToKeepUpKeyPath = NSStringFromSelector(@selector(playbackLikelyToKeepUp));
-    playbackBufferFullKeyPath = NSStringFromSelector(@selector(playbackBufferFull));
+    playbackBufferEmptyKeyPath = NSStringFromSelector(@selector(isPlaybackBufferEmpty));
+    playbackLikelyToKeepUpKeyPath = NSStringFromSelector(@selector(isPlaybackLikelyToKeepUp));
+    playbackBufferFullKeyPath = NSStringFromSelector(@selector(isPlaybackBufferFull));
     
     [self.currentItem addObserver:self forKeyPath:playbackBufferEmptyKeyPath options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:nil];
     [self.currentItem addObserver:self forKeyPath:playbackLikelyToKeepUpKeyPath options:NSKeyValueObservingOptionNew | NSKeyValueObservingOptionOld context:nil];
